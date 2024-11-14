d731475f6fb881df0fc28d0a762f088ec7fb7d72
배민수 <duecost@gmail.com>
2024-11-14 11:59:02 +0900

    init: 프로젝트 설정

---
+++ src/main/java/cau/cau_be/CauBeApplication.java
@@ -0,0 +1,13 @@
+package cau.cau_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CauBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CauBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cau/cau_be/CauBeApplicationTests.java
@@ -0,0 +1,13 @@
+package cau.cau_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CauBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
