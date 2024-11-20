f6322de821b0b097d4ac125f621528e688e4a9de
배민수 <duecost@gmail.com>
2024-11-20 18:12:23 +0900

    fix: 연관관계 매핑 수정

--- src/main/java/cau/cau_be/tech/entity/TechDescription.java
+++ src/main/java/cau/cau_be/tech/entity/TechDescription.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class TechDescription {
@@ -18,7 +19,7 @@ public class TechDescription {
   @Column(nullable = false, columnDefinition = "TEXT")
   private final String description;
 
-  @ManyToOne
+  @OneToOne
   @JoinColumn(name = "tech_id", nullable = false)
   private final Tech tech;
 
